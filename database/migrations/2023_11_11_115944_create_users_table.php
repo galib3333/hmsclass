94e8745d6a715fdf1162519c9c48e53251e53baa
Ibrahim Khalil <iksakil2@gmail.com>
2023-11-20 13:22:05 +0600

    User Done.

--- database/migrations/2023_11_11_115944_create_users_table.php
+++ database/migrations/2023_11_11_115944_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->integer('employ_id')->unsigned();
+            $table->integer('employ_id')->unsigned()->nullable();
             $table->foreign('employ_id')->references('id')->on('employ_basics');
             $table->string('email')->unique()->nullable();
             $table->string('contact_no_en')->unique();
