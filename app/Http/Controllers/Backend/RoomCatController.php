7225307b4709417ddb8d1e3dc4c4b95caf3ea6b8
galib333 <mohapurush6@gmail.com>
2023-11-29 09:11:35 +0600

    Room Cat Added

--- app/Http/Controllers/Backend/RoomCatController.php
+++ app/Http/Controllers/Backend/RoomCatController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 
 use App\Models\RoomCat;
-use App\Http\Requests\StoreRoomCatRequest;
-use App\Http\Requests\UpdateRoomCatRequest;
+use App\Http\Requests\Backend\RoomCat\StoreRoomCatRequest;
+use App\Http\Requests\Backend\RoomCat\UpdateRoomCatRequest;
 
 class RoomCatController extends Controller
 {
@@ -15,7 +15,8 @@ class RoomCatController extends Controller
      */
     public function index()
     {
-        //
+        $roomCat = RoomCat::paginate(10);
+        return view('backend.roomCat.index', compact('roomCat'));
     }
 
     /**
@@ -23,7 +24,7 @@ class RoomCatController extends Controller
      */
     public function create()
     {
-        //
+        return view('backend.roomCat.create');
     }
 
     /**
@@ -31,7 +32,25 @@ class RoomCatController extends Controller
      */
     public function store(StoreRoomCatRequest $request)
     {
-        //
+        try {
+            $roomCat = new Blood();
+            $roomCat->room_cat_name = $request->roomCatName;
+            $roomCat->status = $request->status;
+
+            $roomCat->created_by = currentUserId();
+            if ($roomCat->save()) {
+                $this->notice::success('Room Category Successfully Added');
+                return redirect()->route('roomCat.index');
+            } else {
+                $this->notice::error('Please try again');
+                return redirect()->back();
+            }
+
+        } catch (Exception $e) {
+            dd($e);
+            $this->notice::error('Please try again');
+            return redirect()->back();
+        }
     }
 
     /**
@@ -45,24 +64,47 @@ class RoomCatController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(RoomCat $roomCat)
+    public function edit(RoomCat $id)
     {
-        //
+        $roomCat = RoomCat::findOrFail(encryptor('decrypt', $id));
+        return view('backend.roomCat.edit', compact('roomCat'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateRoomCatRequest $request, RoomCat $roomCat)
+    public function update(UpdateRoomCatRequest $request, RoomCat $id)
     {
-        //
+        try {
+            $blood = Blood::findOrFail(\encryptor('decrypt', $id));
+            $roomCat->room_cat_name = $request->roomCatName;
+            $roomCat->status = $request->status;
+
+            $roomCat->created_by = currentUserId();
+            if ($roomCat->save()) {
+                $this->notice::success('Room Category Successfully Updated');
+                return redirect()->route('roomCat.index');
+            } else {
+                $this->notice::error('Please try again');
+                return redirect()->back();
+            }
+
+        } catch (Exception $e) {
+            dd($e);
+            $this->notice::error('Please try again');
+            return redirect()->back();
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(RoomCat $roomCat)
+    public function destroy(RoomCat $id)
     {
-        //
+        $roomCat = RoomCat::findOrFail(encryptor('decrypt', $id));
+        if ($roomCat->delete()) {
+            $this->notice::error('Room Category Deleted Permanently!');
+            return redirect()->back();
+        }
     }
 }
--- app/Http/Requests/Backend/RoomCat/StoreRoomCatRequest.php
+++ app/Http/Requests/Backend/RoomCat/StoreRoomCatRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Backend\RoomCat;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateRoomCatRequest extends FormRequest
+class StoreRoomCatRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
