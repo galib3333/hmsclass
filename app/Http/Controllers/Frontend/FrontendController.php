ea20e75ba5d30dbfbcab348ae54e974570871070
Asasullah Al Galib <asadullahgalib053@gmail.com>
2023-12-10 10:14:14 +0600

    BF

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -10,6 +10,7 @@ use App\Models\EmployBasic;
 use App\Models\Blood;
 use Exception;
 use App\Http\Requests\Backend\Patient\StorePatientRequest;
+use App\Http\Requests\Backend\Appointment\StoreAppointmentRequest;
 class FrontendController extends Controller
 {
     public function index(){
@@ -66,5 +67,36 @@ class FrontendController extends Controller
         $patient = Patient::get();
         $employee = EmployBasic::get();
         return view('frontend.home', compact('patient', 'employee'));    
+    }
+
+    public function appStore(StoreAppointmentRequest $request)
+    {
+        try {
+            $patient = new Patient();
+            $patient->patient_id = $request->patientId;
+            $patient->name_en = $request->patientNameEN;
+            $patient->name_bn = $request->patientNameBN;
+            $patient->email = $request->emailAddress;
+            $patient->contact_no_en = $request->contactNumber_en;
+            $patient->contact_no_bn = $request->contactNumber_bn;
+            $patient->present_address = $request->presentAddress;
+            $patient->permanent_address = $request->permanentAddress;
+            $patient->birth_date = $request->birthDate;
+            $patient->gender = $request->gender;
+            $patient->blood_id = $request->bloodId;
+            $patient->status = $request->status;
+            $patient->created_by = currentUserId();
+            if ($patient->save()) {
+                $this->notice::success('Successfully Saved Patient. Patient ID: ' . $patient->patient_id);
+                return redirect()->route('frontend.home');
+            } else {
+                $this->notice::error('Please try again');
+                return redirect()->back()->withInput();
+            }
+        } catch (Exception $e) {
+            dd($e);
+            $this->notice::error('Please try again');
+            return redirect()->back()->withInput();
+        }
     }
 }
