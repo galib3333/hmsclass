054b773ecb5bfd4181817e2805c8cd9573d639be
Asasullah Al Galib <asadullahgalib053@gmail.com>
2023-12-17 11:07:02 +0600

    BUG FIX

--- app/Models/EmployBasic.php
+++ app/Models/EmployBasic.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Blood;
+use App\Models\Role;
+use App\Models\Doctor;
 
 class EmployBasic extends Model
 {
@@ -20,5 +22,9 @@ class EmployBasic extends Model
     public function blood()
     {
         return $this->belongsTo(Blood::class, 'blood_id');
+    }
+    public function doctor()
+    {
+        return $this->hasMany(Doctor::class, 'employ_id');
     }
 }
